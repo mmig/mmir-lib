c829cdfa25354615d56afee6fba1a543da7d3495
russa <aaron.russ@dfki.de>
2019-01-25 18:36:41 +0100

    use new require-configuration module instead of "duplication" requirejs.build.base-config.js

--- requirejs-main.js
+++ requirejs-main.js
@@ -1,37 +1,42 @@
 
-//bootstrapping for requirejs
+//bootstrapping for requirejs in node environment
 
 var path = require('path');
 
 try {
 	global.Worker = require('webworker-threads').Worker;
 } catch(err) {
+	console.log(err)
 	console.log('[INFO] module "webworker-threads" not installed: not using WebWorkers/parallel threads for mmir...');
 	global.Worker = void(0);
 }
 
 require('./core.js');
-
 var mmirCore = global.mmir;
-mmirCore._mmirLibPath = __dirname + path.sep;
-mmirCore.basePath = __dirname + path.sep;
 
-// requirejs('mmirf/mainConfig');
+var config = require('./modulesBaseConfig.js');
 
-var config = require('./requirejs.build.base-config.js');
-for(var n in config.paths){
-	config.paths[n] = path.resolve(__dirname, config.paths[n] + '.js').replace(/\.js$/, '');
+var mmirRoot = __dirname;
+
+mmirCore._mmirLibPath = mmirCore._mmirLibPath || mmirRoot + path.sep;
+
+var paths = config.paths;
+for(var n in paths){
+	paths[n] = path.resolve(mmirRoot, paths[n] + '.js').replace(/\.js$/, '');
 }
-for(var i = config.packages.length - 1; i >= 0; --i){
-	config.packages[i].location = path.resolve(__dirname, config.packages[i].location);
+var packages = config.packages;
+for(var i = packages.length - 1; i >= 0; --i){
+	packages[i].location = path.resolve(mmirRoot, packages[i].location);
 }
-config.baseUrl = __dirname;
+
+config.config['mmirf/constants'] = {
+	mmirBasePath: mmirRoot + path.sep
+};
 
 //scion-lib for node env:
 config.paths['mmirf/scion'] = path.resolve(__dirname, 'env/node/scion.js').replace(/\.js$/, '');
 
-// console.log('rqjs -> ', requirejs.config(config));
-// console.log('define -> ', requirejs.define);
+var mmirInitFunc = require('./mainConfigInit.js');
 
 var coreModule = {
 	mmir: mmirCore,
@@ -42,32 +47,9 @@ var coreModule = {
 		global.requirejs = requirejs;
 		coreModule.requirejs = requirejs;
 		var req = requirejs.config(config);
-		mmirCore.require = req;
-		mmirCore._define = requirejs.define;
-		req('mmirf/main');
-		return req('mmirf/core');
+		var core = mmirInitFunc(mmirCore, config, req, requirejs.define);
+		return mmirCore.require(mmirCore.startModule);
 	}
 };
 
 module.exports = coreModule;
-
-
-// coreModule.init();//TEST
-
-// var appConfig = require('build-tool/webpack-app-config');
-// require('build-tool/webpack-helper-module-config').init(appConfig);
-//
-// //trigger initialization
-// require('mmirf/logger');
-// require('mmirf/main');
-// module.exports = require('mmirf/core');
-//
-// if(appConfig.applyIncludes){
-// 	appConfig.applyIncludes();
-// }
-
-// //FIXME TEST:
-// require('mmirf/asyncGrammar');
-// require('mmirf/jsccAsyncGen');
-// require('mmirf/jisonAsyncGen');
-// require('mmirf/pegjsAsyncGen');
