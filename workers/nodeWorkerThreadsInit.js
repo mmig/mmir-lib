afa507662fb2ad0a20c4b16011bb4fdf0d1dbcc2
russa <aaron.russ@dfki.de>
2019-05-20 22:38:56 +0200

    FIX need to guard against webpack build

--- workers/nodeWorkerThreadsInit.js
+++ workers/nodeWorkerThreadsInit.js
@@ -1,47 +1,52 @@
 
-// global.WEBPACK_BUILD = true;
-var worker_threads = require('worker_threads');
-var parentPort = worker_threads.parentPort;
-global.self = this;
-self.isMainThread = worker_threads.isMainThread;
+//ignore for WEBPACK build:
+if(typeof WEBPACK_BUILD === 'undefined'  || !WEBPACK_BUILD){
 
-// var destroyed = false;
-// parentPort.on('exit', function(){
-// 	log('## [worker_threads::onexit] ')
-// 	destroyed = true;
-// });
-// parentPort.on('error', function(err){
-// 	log('## [worker_threads::onerr] ', err);
-// });
-//
-// var log = function(){
-// 	try{
-// 		console.log.apply(console, arguments);
-// 	} catch(_err){}
-// }
+	// global.WEBPACK_BUILD = true;
+	var worker_threads = require('worker_threads');
+	var parentPort = worker_threads.parentPort;
+	global.self = this;
+	self.isMainThread = worker_threads.isMainThread;
 
-Object.defineProperty(global.self, 'onmessage', {
-	set: function(func){
-		parentPort.on('message', function(data){
-			// if(!destroyed) log('## [worker_threads::onmessage] ', data);
-			func.call(global, {data: data});
-		});
-	}
-});
-self.postMessage = function(data){
-	// if(!destroyed) log('## [worker_threads::postMessage] ', data);
-	parentPort.postMessage(data);
-};
+	// var destroyed = false;
+	// parentPort.on('exit', function(){
+	// 	log('## [worker_threads::onexit] ')
+	// 	destroyed = true;
+	// });
+	// parentPort.on('error', function(err){
+	// 	log('## [worker_threads::onerr] ', err);
+	// });
+	//
+	// var log = function(){
+	// 	try{
+	// 		console.log.apply(console, arguments);
+	// 	} catch(_err){}
+	// }
 
-var path = require('path');
-
-global.importScripts = function(){
-	var p;
-	for(var i=0, size = arguments.length; i < size; ++i){
-		p = arguments[i];
-		if(!path.isAbsolute(p) && !/^[^/]+:\/\//.test(p)){
-			p = path.join(__dirname, p);
+	Object.defineProperty(global.self, 'onmessage', {
+		set: function(func){
+			parentPort.on('message', function(data){
+				// if(!destroyed) log('## [worker_threads::onmessage] ', data);
+				func.call(global, {data: data});
+			});
 		}
-		require(p);
+	});
+	self.postMessage = function(data){
+		// if(!destroyed) log('## [worker_threads::postMessage] ', data);
+		parentPort.postMessage(data);
+	};
+
+	var path = require('path');
+
+	global.importScripts = function(){
+		var p;
+		for(var i=0, size = arguments.length; i < size; ++i){
+			p = arguments[i];
+			if(!path.isAbsolute(p) && !/^[^/]+:\/\//.test(p)){
+				p = path.join(__dirname, p);
+			}
+			require(p);
+		}
 	}
+
 }
