e0161bc7fdb13a68466aa40227cd03ebb965ce51
russa <aaron.russ@dfki.de>
2019-08-06 16:15:58 +0200

    include *.ts (in root) and *.css files in lint tasks

--- scripts/gulp.js
+++ scripts/gulp.js
@@ -8,7 +8,9 @@ var fs = require('fs');
 var root = '../';
 
 var src = [
+	root + 'lib/../*.ts',
 	root + 'lib/**/*.js',
+	root + 'lib/**/*.css',
 	'!' + root + 'lib/vendor/libs',
 	'!' + root + 'lib/vendor/libs/**',
 	'!' + root + 'lib/mvc/parser/gen',
@@ -17,32 +19,31 @@ var src = [
 
 var reportFile = root + 'editorconfig-report.log';
 var testDest = root + 'fixed';
-var fixDest = root + 'lib';
 
 /** show editorconfig report */
 gulp.task('check', function() {
-  return gulp.src(src)
+	return gulp.src(src)
 		.pipe(eclint.check())
 		.pipe(reporter());
 });
 
 /** write editorconfig report */
 gulp.task('report', function() {
-  return gulp.src(src)
+	return gulp.src(src)
 		.pipe(eclint.check())
 		.pipe(reporter({output: fs.createWriteStream(reportFile, 'utf8')}));
 });
 
 /** test editorconfig-fix by writing files into sub-dir /fixed/** */
 gulp.task('test', function() {
-  return gulp.src(src)
+	return gulp.src(src)
 		.pipe(eclint.fix())
 		.pipe(gulp.dest(testDest));
 });
 
 /** fix with editorconfig-settings in-place */
 gulp.task('fix', function() {
-  return gulp.src(src)
+	return gulp.src(src, {base: root})
 		.pipe(eclint.fix())
-		.pipe(gulp.dest(fixDest));
+		.pipe(gulp.dest(root));
 });
