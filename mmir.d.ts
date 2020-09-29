b31aff577669bedfa3a87a2c5bc94617e9b2377d
russa <aaron.russ@dfki.de>
2020-09-29 20:48:11 +0200

    added typing for `mmirf/logger` (interfaces `LoggerModule` and `Logger`)

--- mmir.d.ts
+++ mmir.d.ts
@@ -477,6 +477,113 @@ export interface MediaManager {
 	_ready: (moduleName: string) => void;
 }
 
+export interface Logger {
+
+	verbose(msg: string): void;
+	verbose(className: string, msg?: string): void;
+	verbose(className: string, funcName?: string, msg?: string): void;
+	v(msg: string): void;
+	v(className: string, msg?: string): void;
+	v(className: string, funcName?: string, msg?: string): void;
+
+	debug(msg: string): void;
+	debug(className: string, msg?: string): void;
+	debug(className: string, funcName?: string, msg?: string): void;
+	d(msg: string): void;
+	d(className: string, msg?: string): void;
+	d(className: string, funcName?: string, msg?: string): void;
+
+	log(msg: string): void;
+	log(className: string, msg?: string): void;
+	log(className: string, funcName?: string, msg?: string): void;
+	l(msg: string): void;
+	l(className: string, msg?: string): void;
+	l(className: string, funcName?: string, msg?: string): void;
+
+	info(msg: string): void;
+	info(className: string, msg?: string): void;
+	info(className: string, funcName?: string, msg?: string): void;
+	i(msg: string): void;
+	i(className: string, msg?: string): void;
+	i(className: string, funcName?: string, msg?: string): void;
+
+	warn(msg: string): void;
+	warn(className: string, msg?: string): void;
+	warn(className: string, funcName?: string, msg?: string): void;
+	w(msg: string): void;
+	w(className: string, msg?: string): void;
+	w(className: string, funcName?: string, msg?: string): void;
+
+	error(msg: string, error?: any): void;
+	error(className: string, msg?: string, error?: any): void;
+	error(className: string, funcName?: string, msg?: string, error?: any): void;
+	e(msg: string, error?: any): void;
+	e(className: string, msg?: string, error?: any): void;
+	e(className: string, funcName?: string, msg?: string, error?: any): void;
+
+	critical(msg: string, error?: any): void;
+	critical(className: string, msg?: string, error?: any): void;
+	critical(className: string, funcName?: string, msg?: string, error?: any): void;
+	c(msg: string, error?: any): void;
+	c(className: string, msg?: string, error?: any): void;
+	c(className: string, funcName?: string, msg?: string, error?: any): void;
+
+	getLevel(): LogLevelNum;
+	setLevel(loggingLevel: LogLevel | LogLevelNum): void;
+
+	isCritical(): boolean;
+	isDebug(): boolean;
+	isDisabled(): boolean;
+	isError(): boolean;
+	isInfo(): boolean;
+	isVerbose(): boolean;
+	isWarn(): boolean;
+	isc(): boolean;
+	isd(): boolean;
+	ise(): boolean;
+	isi(): boolean;
+	isv(): boolean;
+	isw(): boolean;
+}
+/**
+ * @example
+ * var Logger = mmir.require('mmirf/logger');
+ * var logger = Logger.create('MyClass');
+ */
+export interface LoggerModule {
+
+	create(loggerName: string, logLevel?: LogLevel | LogLevelNum): Logger;
+	create(loggerConfig: {id: string, config(): &{logLevel: LogLevel | LogLevelNum}}): Logger;
+	create(loggerName: string | {id: string, config(): &{logLevel: LogLevel | LogLevelNum}}, logLevel?: LogLevel | LogLevelNum): Logger;
+
+	getDefaultLogLevel(): LogLevelNum;
+	setDefaultLogLevel(loggingLevel: LogLevel | LogLevelNum): void;
+
+	verbose(msg: string): void;
+	verbose(className: string, msg?: string): void;
+	verbose(className: string, funcName?: string, msg?: string): void;
+
+	debug(msg: string): void;
+	debug(className: string, msg?: string): void;
+	debug(className: string, funcName?: string, msg?: string): void;
+
+	log(msg: string): void;
+	log(className: string, msg?: string): void;
+	log(className: string, funcName?: string, msg?: string): void;
+
+	info(msg: string): void;
+	info(className: string, msg?: string): void;
+	info(className: string, funcName?: string, msg?: string): void;
+
+	warn(msg: string): void;
+	warn(className: string, msg?: string): void;
+	warn(className: string, funcName?: string, msg?: string): void;
+
+	error(msg: string, error?: any): void;
+	error(className: string, msg?: string, error?: any): void;
+	error(className: string, funcName?: string, msg?: string, error?: any): void;
+}
+
 export interface TTSOptions {
 		/** text that should be read aloud */
 	text: string | string[];
